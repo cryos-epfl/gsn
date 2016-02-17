e4dc444b067fb85e45ab467d6fc73a1db946ddc8
Nataliya Kryvych <nkryvych@gmail.com>
2016-02-17 11:34:08 +0100

    use GMT+1 timezone

--- src/main/java/gsn/http/datarequest/QueriesBuilder.java
+++ src/main/java/gsn/http/datarequest/QueriesBuilder.java
@@ -1,47 +1,43 @@
 /**
-* Global Sensor Networks (GSN) Source Code
-* Copyright (c) 2006-2014, Ecole Polytechnique Federale de Lausanne (EPFL)
-* 
-* This file is part of GSN.
-* 
-* GSN is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* GSN is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with GSN.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* File: src/gsn/http/datarequest/QueriesBuilder.java
-*
-* @author Ali Salehi
-* @author Timotee Maret
-*
-*/
+ * Global Sensor Networks (GSN) Source Code
+ * Copyright (c) 2006-2014, Ecole Polytechnique Federale de Lausanne (EPFL)
+ *
+ * This file is part of GSN.
+ *
+ * GSN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSN.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File: src/gsn/http/datarequest/QueriesBuilder.java
+ *
+ * @author Ali Salehi
+ * @author Timotee Maret
+ *
+ */
 
 package gsn.http.datarequest;
 
 import gsn.Main;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
 public class QueriesBuilder {
-	
+
 	private static transient Logger 	logger 						= Logger.getLogger(QueriesBuilder.class);
-	
+
 	/* Mandatory Parameters */
 	public static final String 			PARAM_VSNAMES_AND_FIELDS 	= "vsname";
 
@@ -62,26 +58,29 @@ public class QueriesBuilder {
 	protected Map<String, String[]> requestParameters;
 
 	protected SimpleDateFormat sdf = new SimpleDateFormat (Main.getContainerConfig().getTimeFormat());
-	
+
 	private static Hashtable<String, String> allowedTimeFormats = null;
-	
+
 	static {
 		allowedTimeFormats = new Hashtable<String, String> () ;
 		allowedTimeFormats.put("iso","yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 		allowedTimeFormats.put("unix", "unix");
 	}
-	
+
 	public QueriesBuilder (Map<String, String[]> requestParameters) throws DataRequestException {
 		this.requestParameters = requestParameters ;
+		TimeZone zone = TimeZone.getTimeZone("GMT+1");
+		sdf.setTimeZone(zone);
+
 		parseParameters();
 		buildSQLQueries () ;
 	}
-	
+
 	private void parseParameters () throws DataRequestException {
 
 		String[] vsnamesParameters = requestParameters.get(PARAM_VSNAMES_AND_FIELDS);
 
-		if (vsnamesParameters == null) throw new DataRequestException ("You must specify at least one >" + PARAM_VSNAMES_AND_FIELDS + "< parameter.") ; 
+		if (vsnamesParameters == null) throw new DataRequestException ("You must specify at least one >" + PARAM_VSNAMES_AND_FIELDS + "< parameter.") ;
 
 		vsnamesAndStreams = new HashMap<String, FieldsCollection> () ;
 		String name;
@@ -113,11 +112,19 @@ public class QueriesBuilder {
 
 		String lm = getParameter(requestParameters, PARAM_MAX_NB);
 		if (lm != null) limitCriterion = new LimitCriterion (lm);
-		
+
 		String timeformat = getParameter(requestParameters, PARAM_TIME_FORMAT);
 		if (timeformat != null && allowedTimeFormats.containsKey(timeformat)) {
 			String format = allowedTimeFormats.get(timeformat);
-			sdf = format.compareToIgnoreCase("unix") == 0 ? null : new SimpleDateFormat(format) ;
+			if (format.compareToIgnoreCase("unix") == 0) {
+				sdf = null;
+			} else {
+				sdf = new SimpleDateFormat(format);
+				TimeZone zone = TimeZone.getTimeZone("GMT+1");
+				sdf.setTimeZone(zone);
+			}
+
+//			sdf = format.compareToIgnoreCase("unix") == 0 ? null : new SimpleDateFormat(format) ;
 		}
 	}
 
